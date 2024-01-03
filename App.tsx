4d5b8058aa6e6979028487c18f4f05ab2368dab8
GeluHorotan <horotangelu17@gmail.com>
2024-01-03 21:05:56 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -44,6 +44,16 @@ function Section({children, title}: SectionProps): React.JSX.Element {
       </Text>
       <Text
         style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {' '}
+        TRUE TRUE TRUE
+      </Text>
+      <Text
+        style={[
           styles.sectionDescription,
           {
             color: isDarkMode ? Colors.light : Colors.dark,
