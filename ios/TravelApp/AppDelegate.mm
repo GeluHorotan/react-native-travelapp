a2a1c6ab2e52c76648dccab2f4120237f08a061c
GeluHorotan <horotangelu17@gmail.com>
2024-01-08 19:05:59 +0200

    feat: add api key for ios

--- ios/TravelApp/AppDelegate.mm
+++ ios/TravelApp/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"TEST"]; // add this line using the api key obtained from Google Console
+  [GMSServices provideAPIKey:@"AIzaSyDmelqmGRUPbJxy-4HQEAk5tM0p7XcAXHM"]; // add this line using the api key obtained from Google Console
 
   self.moduleName = @"TravelApp";
   // You can add your custom initial props in the dictionary below.
