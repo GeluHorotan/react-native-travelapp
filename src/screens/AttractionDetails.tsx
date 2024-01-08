de849b6b214323392cf391998aac1019fefe1bd6
GeluHorotan <horotangelu17@gmail.com>
2024-01-08 15:29:27 +0200

    feat: create mini image footer gallery

--- src/screens/AttractionDetails.tsx
+++ src/screens/AttractionDetails.tsx
@@ -5,6 +5,9 @@ import {
   StyleSheet,
   Text,
   Dimensions,
+  View,
+  Image,
+  Pressable,
 } from 'react-native';
 
 interface IAttractionDetails {
@@ -24,11 +27,32 @@ const AttractionDetails: FC<IAttractionDetails> = ({ route, navigation }) => {
       <ImageBackground
         style={styles.mainImage}
         source={{ uri: mainImage }}
-        imageStyle={{ borderRadius: 20 }}
-      />
-      <Text onPress={onBack} style={{ margin: 32 }}>
-        BACK
-      </Text>
+        imageStyle={{ borderRadius: 20 }}>
+        <View style={styles.header}>
+          <Pressable onPress={onBack} hitSlop={8}>
+            <Image style={styles.icon} source={require('../assets/back.png')} />
+          </Pressable>
+          <Pressable hitSlop={8}>
+            <Image
+              style={styles.icon}
+              source={require('../assets/share.png')}
+            />
+          </Pressable>
+        </View>
+
+        <View style={styles.footer}>
+          {item?.images?.length
+            ? item?.images?.map(image => (
+                <Image
+                  key={image}
+                  style={styles.miniImage}
+                  source={{ uri: image }}
+                />
+              ))
+            : null}
+        </View>
+      </ImageBackground>
+
       <Text>{item?.name}</Text>
     </SafeAreaView>
   );
@@ -43,6 +67,35 @@ const styles = StyleSheet.create({
   mainImage: {
     width: '100%',
     height: height / 2,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 36,
+    height: 36,
+    margin: 16,
+  },
+  footer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    backgroundColor: 'rgba(255,255,255,0.35)',
+    margin: 16,
+    paddingHorizontal: 8,
+  },
+  miniImage: {
+    width: 40,
+    height: 40,
+    margin: 8,
+    borderRadius: 10,
   },
 });
 
